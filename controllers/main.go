782253f5fa2d81d24e8a091a30f531fb704aebe4
Cracker <cracker@Crackers-MacBook-Pro.local>
2021-07-22 15:19:09 +0700

    Test01 Send notify to line group

--- controllers/main.go
+++ controllers/main.go
@@ -1,10 +1,17 @@
 package controllers
 
 import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/Cracker-TG/line-notify-covid19-report/forms"
 	"github.com/Cracker-TG/line-notify-covid19-report/helpers"
+	"github.com/enescakir/emoji"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,12 +19,45 @@ type MainController struct{}
 
 var dataForm = new(forms.BodyData)
 var h helpers.HelpersInteface = new(helpers.Helpers);
+
+type ResponseLine struct {
+	Status int `json:"status"`
+	Message string `json:"message"`
+}
+
 func (mc MainController) PushNoti(c *gin.Context) {
 	key := os.Getenv("COVID_KEY")
+	token_line := os.Getenv("COVID_TOKEN_LINE_NOTI")
+
 	err := h.GetJson("https://api-lab-covid.mindbase.co.th/v2/stats/daily?key=" + key, dataForm)
 	if err != nil {
 		c.JSON(500, gin.H{"status": err})
 	}
 
-	c.JSON(200, gin.H{"status": dataForm})
+	endpoint := "https://notify-api.line.me/api/notify"
+	data := url.Values{}
+	data.Set("message", "โควิดวันนี้ " + strconv.Itoa(dataForm.Data.DailyCovidCases) + string(emoji.PileOfPoo))
+
+	client := &http.Client{}
+	r, err := http.NewRequest("POST", endpoint, strings.NewReader(data.Encode()))
+
+	if err != nil {
+			log.Fatal(err)
+	}
+
+	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	r.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+	r.Header.Add("Authorization", "Bearer " + token_line)
+
+	res, err := client.Do(r)
+	if err != nil {
+			log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	result := new(ResponseLine)
+	json.NewDecoder(res.Body).Decode(result)
+
+	c.JSON(200, gin.H{"status": result.Status, "message": result.Message})
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Cracker-TG/line-notify-covid19-report
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.2 // indirect
+require (
+	github.com/enescakir/emoji v1.0.0 // indirect
+	github.com/gin-gonic/gin v1.7.2 // indirect
+)
